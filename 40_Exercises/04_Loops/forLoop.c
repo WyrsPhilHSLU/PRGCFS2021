ff8376f758ac84e42c2df5744122207c298673d3
Wyrsch Philipp <fips@home.com>
2021-03-25 22:49:28 +0100

    SW 05

--- 40_Exercises/04_Loops/forLoop.c
+++ 40_Exercises/04_Loops/forLoop.c
@@ -1,32 +1,81 @@
 #include <stdio.h>
 
-
-/*
-#define TASK_NUM 6
-
-int main(int argc, char *argv[])
+int main()
 {
-	//all used for task 4
 	int sum = 0;
-	int start = 1;
-	int end = 10;
+	int i = 1;
+	int maxSum = 555;
 	
-	switch(TASK_NUM)
+	for( i = 1; ; i++)
 	{
-		
-	for( int i = start; i <= end; (i = i + inkrement))
-	{
-		sum = sum + i;
+		if( sum >= maxSum)
+		{
+			break;
+		}
+		sum += i;
 	}
 
-	printf("The sum from %d to %d is equal to %d\n", start, end, sum);
-
+	printf("The sum from %d to %d is equal to %d\n", start, maxSum, sum);
+	
 	return 0;
+}	
+	
+	
+/*	//zeigt 1 - 100 in Zehnerfolge mit Abschnitten
+{
+	int start = 0;
+	int end = 90;
+	int ten = 10;
+	int one = 1;
+	
+		
+	for( int i = start; i <= end; ( i = i + ten))
+	{	
+		for( int k = 1; k <= 10; ( k = k + one))
+		{
+			printf("%d ", k+i);
+		}
+	
+		printf("\n");
 	}
+	
+	return 0;
 }
 */
 
-int main()
+/*	//zeigt jeden wert bis 729, wenn man immer mal 3 rechnet, beginnend mit 1
+{
+	int start = 1;
+	int end = 729;
+	int multi = 3;
+	
+	for( int i = start; i <= end; ( i = i * multi))
+	{
+		printf("%d\n", i);
+	}
+	
+	return 0;
+}
+*/
+
+/*
+	//zeigt jeden wert von 40 bis 25 einzeln
+{
+	int show = 40;
+	int start = 40;
+	int end = 25;
+	int inkrement = 1;
+	
+	for( int i = start; i >= end; ( i = i - inkrement))
+	{
+		printf("%d\n", i);
+	}
+	
+	return 0;	
+}
+
+
+	//summiert von 2 bis 10 alle Zahlen
 {
 	int sum = 0;
 	int start = 2;
@@ -42,3 +91,4 @@ int main()
 
 	return 0;
 }
+*/
