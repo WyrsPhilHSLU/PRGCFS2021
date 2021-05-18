8367a5275469d046c9364c2097dc08763c5b9ca8
Wyrsch Philipp <fips@home.com>
2021-05-18 16:40:34 +0200

    Testat

---
+++ 90_Testat/Testat_2.c
@@ -0,0 +1,60 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+struct DataFrame {
+	long long timeStamp;
+ 	int pressure;
+ 	char systemState;
+ 	char alarmState;
+};
+
+long long wert_timeStamp;
+int wert_pressure;
+char wert_systemState;
+char wert_alarmState;
+
+char str[5];
+
+int a, b, c, d;
+
+int main()
+{
+	FILE *pBin= fopen("pressureSpike.bin", "rb");
+	FILE *pCsv= fopen("pressureSpike.csv", "w");
+	
+	if( pBin != NULL )
+	{
+		const int BYTES_IN_LINE = 14;
+		unsigned char lineBuffer[BYTES_IN_LINE]; 
+		fprintf(pCsv,"Time Stamp, Pressure [Pa], System State, Alarm State\n");
+		 
+		while(fread(lineBuffer,BYTES_IN_LINE, 1, pBin) )
+		{
+			wert_timeStamp = ((struct DataFrame *)lineBuffer)->timeStamp;
+			
+			time_t rawtime = wert_timeStamp/1000 ;
+			
+			struct tm ts;
+			char buf[80];
+			
+			ts=*localtime(&rawtime);
+			
+			strftime(buf,sizeof(buf),"%A %d-%m-%Y %X %Z %z",&ts);
+			
+			wert_pressure = ((struct DataFrame *)lineBuffer)->pressure;
+			
+			wert_systemState = ((struct DataFrame *)lineBuffer)->systemState;
+			
+			wert_alarmState = ((struct DataFrame *)lineBuffer)->alarmState;
+			
+			fprintf(pCsv,"%s,%d,%d,%d\n",buf,wert_pressure,wert_systemState,wert_alarmState);
+			
+		}
+
+	}
+	fclose(pBin);
+	fclose(pCsv);
+	return 0;
+}
+	
